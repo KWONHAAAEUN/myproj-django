d3d95259c6e34721a8b70ae01084f4bc9ddfdd66
kwonhaeun <wooyoung_000@naver.com>
2022-01-14 17:49:25 +0900

    검색 기능을 위한 코드

--- lol/views.py
+++ lol/views.py
@@ -7,3 +7,12 @@ from lol.serializers import PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset=Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_queryset(self):
+        qs= super().get_queryset()
+
+        query=self.request.query_params.get("query","")
+        if query:
+            qs=qs.filter(champion__icontains=query)
+
+        return qs
